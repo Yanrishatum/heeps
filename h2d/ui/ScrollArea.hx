0af0e542c445e4d304582424d38d4b132d3b8bd1
Yanrishatum <krabanek@gmail.com>
2019-02-02 23:18:38 +0300

    Spaces and fixes.

--- h2d/ui/ScrollArea.hx
+++ h2d/ui/ScrollArea.hx
@@ -12,11 +12,15 @@ class ScrollArea extends Mask
   public var scrollX(default, set):Float = 0;
   public var scrollY(default, set):Float = 0;
   
-  public var scrollVertically:Bool = true;
-  public var scrollHorizontally:Bool = true;
+  // Are disregarded for not, so let's not confuse people.
+  // public var scrollVertically:Bool = true;
+  // public var scrollHorizontally:Bool = true;
   
   public var scrollBounds:Bounds;
   
+  /**
+    A scroll step when scrolled via `scrollBy`
+  **/
   public var scrollStep:Int;
   
   public function new(width:Int, height:Int, scrollStep:Int = 16, ?bounds:Bounds, ?parent:Object)
@@ -39,12 +43,18 @@ class ScrollArea extends Mask
     posChanged = true;
     return scrollY = v;
   }
-	
-	function scrollBy(deltaX:Float, deltaY:Float):Void
-	{
-		scrollX += deltaX * scrollStep;
-		scrollY += deltaY * scrollStep;
-	}
+  
+  function scrollBy(deltaX:Float, deltaY:Float):Void
+  {
+    scrollX += deltaX * scrollStep;
+    scrollY += deltaY * scrollStep;
+  }
+  
+  function scrollPixels(x:Float, y:Float):Void
+  {
+    scrollX += x;
+    scrollY += y;
+  }
   
   override private function calcAbsPos()
   {
@@ -52,76 +62,84 @@ class ScrollArea extends Mask
     absX -= scrollX;
     absY -= scrollY;
   }
-	
-	override public function globalToLocal(pt:Point):Point
-	{
-		return super.globalToLocal(pt);
-		pt.x += scrollX;
-		pt.y += scrollY;
-	}
   
-	override function drawRec( ctx : h2d.RenderContext ) @:privateAccess {
-		if( !visible ) return;
-		// fallback in case the object was added during a sync() event and we somehow didn't update it
-		if( posChanged ) {
-			// only sync anim, don't update() (prevent any event from occuring during draw())
-			// if( currentAnimation != null ) currentAnimation.sync();
-			calcAbsPos();
-			for( c in children )
-				c.posChanged = true;
-			posChanged = false;
-		}
+  // override public function localToGlobal(?pt:Point):Point
+  // {
+  // 	pt = super.localToGlobal(pt);
+  // 	pt.x 
+  // 	return pt;
+  // }
+  
+  // override public function globalToLocal(pt:Point):Point
+  // {
+  // 	pt = super.globalToLocal(pt);
+  // 	pt.x += scrollX;
+  // 	pt.y += scrollY;
+  // 	return pt;
+  // }
+  
+  override function drawRec( ctx : h2d.RenderContext ) @:privateAccess {
+    if( !visible ) return;
+    // fallback in case the object was added during a sync() event and we somehow didn't update it
+    if( posChanged ) {
+      // only sync anim, don't update() (prevent any event from occuring during draw())
+      // if( currentAnimation != null ) currentAnimation.sync();
+      calcAbsPos();
+      for( c in children )
+        c.posChanged = true;
+      posChanged = false;
+    }
     
-		var x1 = absX + scrollX;
-		var y1 = absY + scrollY;
+    var x1 = absX + scrollX;
+    var y1 = absY + scrollY;
 
-		var x2 = width * matA + height * matC + x1;
-		var y2 = width * matB + height * matD + y1;
+    var x2 = width * matA + height * matC + x1;
+    var y2 = width * matB + height * matD + y1;
 
-		var tmp;
-		if (x1 > x2) {
-			tmp = x1;
-			x1 = x2;
-			x2 = tmp;
-		}
+    var tmp;
+    if (x1 > x2) {
+      tmp = x1;
+      x1 = x2;
+      x2 = tmp;
+    }
 
-		if (y1 > y2) {
-			tmp = y1;
-			y1 = y2;
-			y2 = tmp;
-		}
+    if (y1 > y2) {
+      tmp = y1;
+      y1 = y2;
+      y2 = tmp;
+    }
 
-		ctx.flush();
-		if( ctx.hasRenderZone ) {
-			var oldX = ctx.renderX, oldY = ctx.renderY, oldW = ctx.renderW, oldH = ctx.renderH;
-			ctx.setRenderZone(x1, y1, x2-x1, y2-y1);
-			objDrawRec(ctx);
-			ctx.flush();
-			ctx.setRenderZone(oldX, oldY, oldW, oldH);
-		} else {
-			ctx.setRenderZone(x1, y1, x2-x1, y2-y1);
-			objDrawRec(ctx);
-			ctx.flush();
-			ctx.clearRenderZone();
-		}
-	}
+    ctx.flush();
+    if( ctx.hasRenderZone ) {
+      var oldX = ctx.renderX, oldY = ctx.renderY, oldW = ctx.renderW, oldH = ctx.renderH;
+      ctx.setRenderZone(x1, y1, x2-x1, y2-y1);
+      objDrawRec(ctx);
+      ctx.flush();
+      ctx.setRenderZone(oldX, oldY, oldW, oldH);
+    } else {
+      ctx.setRenderZone(x1, y1, x2-x1, y2-y1);
+      objDrawRec(ctx);
+      ctx.flush();
+      ctx.clearRenderZone();
+    }
+  }
   
   function objDrawRec(ctx:h2d.RenderContext) {
-		if( filter != null && filter.enable ) {
-			drawFilters(ctx);
-		} else {
-			var old = ctx.globalAlpha;
-			ctx.globalAlpha *= alpha;
-			if( ctx.front2back ) {
-				var nchilds = children.length;
-				for (i in 0...nchilds) children[nchilds - 1 - i].drawRec(ctx);
-				draw(ctx);
-			} else {
-				draw(ctx);
-				for( c in children ) c.drawRec(ctx);
-			}
-			ctx.globalAlpha = old;
-		}
+    if( filter != null && filter.enable ) {
+      drawFilters(ctx);
+    } else {
+      var old = ctx.globalAlpha;
+      ctx.globalAlpha *= alpha;
+      if( ctx.front2back ) {
+        var nchilds = children.length;
+        for (i in 0...nchilds) children[nchilds - 1 - i].drawRec(ctx);
+        draw(ctx);
+      } else {
+        draw(ctx);
+        for( c in children ) c.drawRec(ctx);
+      }
+      ctx.globalAlpha = old;
+    }
   }
   
 }
