76f394517879f6a965657dc0b8f52ab779ae6b24
Yanrishatum <krabanek@gmail.com>
2020-04-25 01:16:11 +0300

    Relocate h3d -> ch3

---
+++ ch3/scene/Axes.hx
@@ -0,0 +1,28 @@
+package ch3.scene;
+
+import h3d.scene.Object;
+
+class Axes extends Object
+{
+  
+  public function new(?parent:Object)
+  {
+    super(parent);
+    
+    var ax = new h3d.scene.Box(0xFFFF0000, true, this);
+    ax.x = 0.5;
+    ax.scaleY = 0.001;
+    ax.scaleZ = 0.001;
+
+    var ay = new h3d.scene.Box(0xFF00FF00, true, this);
+    ay.y = 0.5;
+    ay.scaleX = 0.001;
+    ay.scaleZ = 0.001;
+
+    var az = new h3d.scene.Box(0xFF0000FF, true, this);
+    az.z = 0.5;
+    az.scaleX = 0.001;
+    az.scaleY = 0.001;
+  }
+  
+}
---
+++ ch3/scene/TileSprite.hx
@@ -0,0 +1,91 @@
+package ch3.scene;
+
+import h3d.Matrix;
+import h3d.scene.RenderContext;
+import h3d.scene.Object;
+import h3d.scene.Mesh;
+import h3d.mat.Material;
+import h2d.Tile;
+import ch3.prim.PlanePrim;
+
+/**
+  A classic 2D sprite that can follow camera orientation to always face it.
+  Sprite front is X forward.
+**/
+@:access(h2d.Tile)
+class TileSprite extends Mesh
+{
+  /**
+    If true, will follow camera orientation to always appear facing X coordinate at camera. (default: true)
+  **/
+  public var faceCamera:Bool;
+  /**
+    If true, will track camera on Z axis, otherwise only X and Y coordinates will be adjusted, and sprite will look forward at all times. (default: true)
+  **/
+  public var faceZAxis:Bool;
+  // TODO: zAxis rotation.
+  /**
+    Currently displayed `h2d.Tile`.
+  **/
+  public var tile(default, set):Tile;
+  var ppu:Float;
+  /**
+    Pixels per unit value. Affects the sprite size in 3D space. (default: 1)
+  **/
+  public var pixelsPerUnit(default, set):Float;
+  var plane:PlanePrim;
+  
+  public function new(tile:Tile, ppu:Float = 1, faceCamera:Bool = true, ?parent:Object)
+  {
+    this.faceCamera = faceCamera;
+    this.faceZAxis = true;
+    this.plane = new PlanePrim(1, 1, X);
+    var mat = Material.create(tile.getTexture());
+    mat.blendMode = Alpha;
+    super(plane, mat, parent);
+    
+    this.pixelsPerUnit = ppu;
+    this.tile = tile;
+    alwaysSync = true;
+  }
+  
+  function set_tile(t:Tile):Tile
+  {
+    if (t == null)
+    {
+      // Do something
+    }
+    else
+    {
+      plane.set(t.width * ppu, t.height * ppu, t.u, t.v, t.u2, t.v2, t.dx * ppu, t.dy * ppu);
+      material.texture = t.getTexture();
+    }
+    return this.tile = t;
+  }
+  
+  function set_pixelsPerUnit(v:Float):Float
+  {
+    ppu = 1 / v;
+    if (tile != null) set_tile(tile);
+    return pixelsPerUnit = v;
+  }
+  
+  override private function syncRec(ctx:RenderContext)
+  {
+    if (faceCamera)
+    {
+      var up = ctx.scene.camera.up;
+      var vec = ctx.scene.camera.pos.sub(ctx.scene.camera.target);
+      if (!faceZAxis) vec.z = 0;
+      var oldX = qRot.x;
+      var oldY = qRot.y;
+      var oldZ = qRot.z;
+      var oldW = qRot.w;
+      qRot.initRotateMatrix(Matrix.lookAtX(vec, up));
+      if (oldX != qRot.x || oldY != qRot.y || oldZ != qRot.z || oldW != qRot.w)
+        this.posChanged = true;
+    }
+    super.syncRec(ctx);
+  }
+  
+}
