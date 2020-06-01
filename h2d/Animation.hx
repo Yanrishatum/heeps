238dc2cd684501cb12f150758bc286aa501277f3
Yanrishatum <krabanek@gmail.com>
2020-06-01 13:05:40 +0300

    Add missing deprecation typedefs

--- h2d/Animation.hx
+++ h2d/Animation.hx
@@ -2,3 +2,5 @@ package h2d;
 
 @:deprecated("Moved to ch2 package")
 typedef Animation = ch2.Animation;
+@:deprecated("Moved to ch2 package")
+typedef AnimationFrame = ch2.Animation.AnimationFrame;
--- h2d/AnimationExt.hx
+++ h2d/AnimationExt.hx
@@ -2,3 +2,7 @@ package h2d;
 
 @:deprecated("Moved to ch2 package")
 typedef AnimationExt = ch2.AnimationExt;
+@:deprecated("Moved to ch2 package")
+typedef AnimationCommand = ch2.AnimationExt.AnimationCommand;
+@:deprecated("Moved to ch2 package")
+typedef AnimationDescriptor = ch2.AnimationExt.AnimationDescriptor;
